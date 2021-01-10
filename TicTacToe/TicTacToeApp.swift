581fa6c1bb8613cb38ef8489d2a45f48ee4cb776
Jonathan Huston <jonathan.huston@gmail.com>
2021-01-10 10:57:25 +0100

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Jonathan Huston on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
